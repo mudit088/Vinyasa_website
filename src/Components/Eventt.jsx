8faf3c0891e4a528c2080d1610f11976831b08ce
mudit088 <muditsri25@gmail.com>
2024-02-25 23:35:13 +0530

    updated

--- src/Components/Eventt.jsx
+++ src/Components/Eventt.jsx
@@ -8,7 +8,7 @@ const Eventt = () => {
         <h1 className=" font-display text-zinc-300 text-center  text-4xl font-extrabold leading-none  tracking-tight md:text-5xl  lg:text-8xl lg:mb-36">
        Events
       </h1>
-        <div className='pb-36'>
+        <div className='pb-36 pt-20 sm:pt-0'>
           <p className='font-bold text-white text-2xl flex items-center justify-center '>
           VSOC - To Be Announced...
           </p>
